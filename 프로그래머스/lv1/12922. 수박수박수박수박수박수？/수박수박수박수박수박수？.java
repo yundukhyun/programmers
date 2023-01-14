1d4b05bd3e4a4a16cb684e9cc6eec8ce56498832
yundukhyun <114922309+yundukhyun@users.noreply.github.com>
2023-01-14 21:14:39 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.27 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        String su = "수";
+        String bak = "박";
+        for (int i = 0; i < n; i++) {
+            if(i%2==0){
+                answer+= su;
+            }else{
+                answer+= bak;
+            }
+
+        }
+        return answer;
+    }
+}
