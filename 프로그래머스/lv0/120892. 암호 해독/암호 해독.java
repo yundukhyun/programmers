f4d5f2fb1383103e6be3750517ab8d793c174cae
yundukhyun <114922309+yundukhyun@users.noreply.github.com>
2023-02-16 22:07:32 +0900

    [level 0] Title: 암호 해독, Time: 3.67 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        for (int i = 1; i <= cipher.length(); i++) {
+
+            if(i %code  == 0){
+                answer += String.valueOf(cipher.charAt(i-1));
+            }
+        }
+        return answer;
+    }
+}
